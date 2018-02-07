aa4d7e66f23daec54a6463d630b71eb03caaf818
dos <osipychev@gmail.com>
2018-02-07 17:53:40 -0600

    pip8 style clean

--- simulation.py
+++ simulation.py
@@ -1,40 +1,37 @@
-##this software is developed for research purpose by Denis Osipychev
+# this software is developed for research purpose by Denis Osipychev
 # DASLAB UIUC 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from const import TeamConst, MapConst 
+from const import TeamConst, MapConst
 from agent import Agent, GroundVehicle, AerialVehicle, GrayAgent
 from gridworld import GridWorld
-    
-##=== SIMULATION STARTS HERE
 
+# SIMULATION STARTS HERE
 gr = GridWorld()
 agents_list = []
 
 for i in range(TeamConst.NUM_RED//2):
-    l = np.random.randint(0,100,[2])
-    agents_list.append(GroundVehicle(l, TeamConst.RED))
-    l = np.random.randint(0,100,[2])
-    agents_list.append(AerialVehicle(l, TeamConst.RED))
+    loc = np.random.randint(0, 100, [2])
+    agents_list.append(GroundVehicle(loc, TeamConst.RED))
+    loc = np.random.randint(0, 100, [2])
+    agents_list.append(AerialVehicle(loc, TeamConst.RED))
 for i in range(TeamConst.NUM_BLUE//2):
-    l = np.random.randint(0,100,[2])
-    agents_list.append(GroundVehicle(l, TeamConst.BLUE))
-    l = np.random.randint(0,100,[2])
-    agents_list.append(AerialVehicle(l, TeamConst.BLUE))
+    loc = np.random.randint(0, 100, [2])
+    agents_list.append(GroundVehicle(loc, TeamConst.BLUE))
+    loc = np.random.randint(0, 100, [2])
+    agents_list.append(AerialVehicle(loc, TeamConst.BLUE))
 for i in range(TeamConst.NUM_GRAY):
-    l = np.random.randint(0,100,[2])
-    agents_list.append(GroundVehicle(l, TeamConst.GRAY))
+    loc = np.random.randint(0, 100, [2])
+    agents_list.append(GroundVehicle(loc, TeamConst.GRAY))
 
 gr.update_map(agents_list)
 
-fig = plt.figure(0,figsize=[15,5])
+fig = plt.figure(0, figsize=[15, 5])
 
 while(plt.fignum_exists(0)):
     for agent in agents_list:
-        agent.move(np.random.randint(0,5))
+        agent.move(np.random.randint(0, 5))
         gr.update_map(agents_list)
     gr.plot_all()
-    
-    
