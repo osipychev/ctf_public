18496120fb6df95b8d921503459e42cc6c8536b0
Zman94 <zgleason94@gmail.com>
2018-02-19 16:37:30 -0600

    Player movement working

--- cap_learn.py
+++ cap_learn.py
@@ -8,14 +8,14 @@ observation = env.reset()
 done = False
 t = 0
 while not done:
-    # action = env.action_space.sample()  # choose random action
-    action = ["S", "X", "X", "X"]
+    action = env.action_space.sample()  # choose random action
+    # action = ["S", "X", "X", "X"]
     observation, reward, done, info = env.step(action)  # feedback from environment
     env.render("human")
     t += 1
     # if not t % 100:
         # print(t, info)
-    time.sleep(3)
+    time.sleep(5)
     if t == 100000:
         break
 print("--- %s seconds ---" % (time.time() - start_time))
