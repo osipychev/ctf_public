cf0a6b1f960e6762099862fc969ad07e148039e1
dos <osipychev@gmail.com>
2018-03-07 16:52:32 -0600

    added three discrete map sizes. use v0,v1,v2 in env

--- gym_cap/gym_cap/__init__.py
+++ gym_cap/gym_cap/__init__.py
@@ -3,5 +3,15 @@ from gym.envs.registration import register
 
 register(
     id='cap-v0',
-    entry_point='gym_cap.envs:CapEnvSample20x20',
+    entry_point='gym_cap.envs:CapEnvGenerate20x20',
+)
+
+register(
+    id='cap-v1',
+    entry_point='gym_cap.envs:CapEnvGenerate100x100',
+)
+
+register(
+    id='cap-v2',
+    entry_point='gym_cap.envs:CapEnvGenerate500x500',
 )
