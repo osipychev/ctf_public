010738facb59470595f90898ba79b76f49279542
dos <osipychev@gmail.com>
2018-03-07 19:25:21 -0600

    agent class moved to a sep file. range made to circle

--- cap_learn.py
+++ cap_learn.py
@@ -3,7 +3,7 @@ import gym
 import gym_cap
 
 start_time = time.time()
-env = gym.make("cap-v1")
+env = gym.make("cap-v0")
 observation = env.reset(0)
 done = False
 t = 0
@@ -12,7 +12,7 @@ while not done:
     # action = [2, 2, 2, 2]
     observation, reward, done, info = env.step(action)  # feedback from environment
     #obs, obs2,  or env
-    env.render(mode="obs")
+    env.render(mode="env")
     t += 1
     # if not t % 100:
         # print(t, info)
