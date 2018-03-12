f9db273775339b3e44d6f2c07813cca73963c551
Zman94 <zgleason94@gmail.com>
2018-03-12 15:51:16 -0500

    moved cap_learn to cap_test. cap_learn has rl algorithm

---
+++ cap_test.py
@@ -0,0 +1,24 @@
+import time
+import gym
+import gym_cap
+
+start_time = time.time()
+env = gym.make("cap-v0")
+#observation = env.reset(0)
+done = False
+t = 0
+
+while not done:
+    action = env.action_space.sample()  # choose random action
+    # action = [2, 2, 2, 2]
+    observation, reward, done, info = env.step(action)  # feedback from environment
+    #obs, obs2,  or env
+    env.render(mode="env")
+    t += 1
+    # if not t % 100:
+        # print(t, info)
+    time.sleep(.25)
+    print(reward)
+    if t == 100000:
+        break
+print("--- %s seconds ---" % (time.time() - start_time))
