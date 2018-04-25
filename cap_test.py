b85a5c110d96ca306e4e6047f21ed975e8d5fe1f
Zman94 <zgleason94@gmail.com>
2018-04-25 11:45:07 -0500

    Moved air units to end of entity list

--- cap_test.py
+++ cap_test.py
@@ -8,16 +8,17 @@ env = gym.make("cap-v0")
 done = False
 t = 0
 
+env.reset()
 while not done:
-    action = env.action_space.sample()  # choose random action
-    # action = [2, 2, 2, 2]
-    observation, reward, done, info = env.step(action, "human")  # feedback from environment
+    # action = env.action_space.sample()  # choose random action
+    action = [4, 4, 4, 4, 4, 4]
+    observation, reward, done, info = env.step(action)  # feedback from environment
     #obs, obs2,  or env
     env.render(mode="env")
     t += 1
     # if not t % 100:
         # print(t, info)
-    time.sleep(.15)
+    time.sleep(.25)
     # print(reward)
     if t == 100000:
         break
