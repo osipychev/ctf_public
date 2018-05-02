9ca254e7d9fa451d1857671a5a9d1b244df2bdc4
Zman94 <zgleason94@gmail.com>
2018-05-02 15:25:17 -0500

    Fixed error checking and UAV movement

--- cap_test.py
+++ cap_test.py
@@ -10,15 +10,20 @@ t = 0
 
 while True:
     while not done:
-        # action = env.action_space.sample()  # choose random action
-        action = (4)*(5**0)+(4)*(5**1)+(4)*(5**2)+(4)*(5**3)+(4)*(5**4)+(4)*(5**5)
+        action = env.action_space.sample()  # choose random action
+        # action = (4)*(5**0) \
+            # +(4)*(5**1) \
+            # +(4)*(5**2) \
+            # +(4)*(5**3) \
+            # +(4)*(5**4) \
+            # +(4)*(5**5)
         observation, reward, done, info = env.step(action, "random")  # feedback from environment
         #obs, obs2,  or env
         env.render(mode="env")
         t += 1
         # if not t % 100:
             # print(t, info)
-        time.sleep(.25)
+        time.sleep(.5)
         # print(reward)
         if t == 100000:
             break
