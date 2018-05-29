52754fc192ae8f316d9172f7e46d2c7cb6fc61e2
Zman94 <zgleason94@gmail.com>
2018-05-29 18:20:05 -0500

    Changed individual reward

--- cap_test.py
+++ cap_test.py
@@ -8,7 +8,7 @@ start_time = time.time()
 env = gym.make("cap-v0")
 done = False
 t = 0
-env.reset(map_size=10, in_seed=4)
+env.reset(map_size=20, mode="random", in_seed=4)
 
 while True:
     while not done:
@@ -47,7 +47,7 @@ while True:
         time.sleep(.05)
         if t == 100000:
             break
-    env.reset(map_size=100, mode="sandbox", in_seed=4)
+    env.reset(map_size=20, mode="random", in_seed=4)
     done = False
 
 # print("--- %s seconds ---" % (time.time() - start_time))
