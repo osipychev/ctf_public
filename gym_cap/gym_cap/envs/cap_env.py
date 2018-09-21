52ba144c4faf54defec4ed87af93316c3c2207c4
dos <osipychev@gmail.com>
2018-09-21 12:01:30 -0500

    fixed mode issue

--- gym_cap/gym_cap/envs/cap_env.py
+++ gym_cap/gym_cap/envs/cap_env.py
@@ -97,6 +97,8 @@ class CapEnv(gym.Env):
         self.create_observation_space()
         self.state = self.observation_space_blue
 
+        self.mode = mode
+
         if NUM_RED == 0:
             self.mode = "sandbox"
 
