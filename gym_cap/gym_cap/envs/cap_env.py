added random territory generator. functions moved. added grey team generator. fixed reward system. fixed bug with rendering - uavs and rotation

gym_cap/gym_cap/envs/cap_env.py
@@ -48,51 +48,21 @@ class CapEnv(gym.Env):
         self.render_mode = render_mode
 
         if map_size is None:
-            self._env = CreateMap.gen_map('map', dim=self.map_size[0])
+            self._env, self.team_home = CreateMap.gen_map('map', dim=self.map_size[0])
         else:
-            self._env = CreateMap.gen_map('map', map_size)
+            self._env, self.team_home = CreateMap.gen_map('map', map_size)
 
         self.map_size = (len(self._env), len(self._env[0]))
-        self.team_home = self._env.copy()
-
-        self.team_blue = []
-        self.team_red = []
 
         if policy_blue is not None: self.policy_blue = policy_blue
         if policy_red is not None: self.policy_red = policy_red
 
-        for y in range(len(self._env)):
-            for x in range(len(self._env[0])):
-                if self._env[x][y] == TEAM1_UGV:
-                    cur_ent = GroundVehicle([x, y], self.team_home, 1)
-                    self.team_blue.insert(0, cur_ent)
-                    self.team_home[x][y] = TEAM1_BACKGROUND
-                elif self._env[x][y] == TEAM1_UAV:
-                    cur_ent = AerialVehicle([x, y], self.team_home, 1)
-                    self.team_blue.append(cur_ent)
-                    self.team_home[x][y] = TEAM1_BACKGROUND
-                elif self._env[x][y] == TEAM2_UGV:
-                    cur_ent = GroundVehicle([x, y], self.team_home, 2)
-                    self.team_red.insert(0, cur_ent)
-                    self.team_home[x][y] = TEAM2_BACKGROUND
-                elif self._env[x][y] == TEAM2_UAV:
-                    cur_ent = AerialVehicle([x, y], self.team_home, 2)
-                    self.team_red.append(cur_ent)
-                    self.team_home[x][y] = TEAM2_BACKGROUND
-
-        # print(DataFrame(self._env))
-        # place arial units at end of list
-        for i in range(len(self.team_blue)):
-            if self.team_blue[i].air:
-                self.team_blue.insert(len(self.team_blue), self.team_blue.pop(i))
-        for i in range(len(self.team_red)):
-            if self.team_red[i].air:
-                self.team_red.insert(len(self.team_red) - 1, self.team_red.pop(i))
-
         self.action_space = spaces.Discrete(len(self.ACTION) ** (NUM_BLUE + NUM_UAV))
 
-        self.game_lost = False
-        self.game_won = False
+        self.blue_win = False
+        self.red_win = False
+
+        self.team_blue, self.team_red = self._map_to_list(self._env, self.team_home)
 
         self.create_observation_space()
         self.state = self.observation_space_blue
@@ -102,8 +72,8 @@ class CapEnv(gym.Env):
         if NUM_RED == 0:
             self.mode = "sandbox"
 
-        self.game_lost = False
-        self.game_won = False
+        self.blue_win = False
+        self.red_win = False
         self.cur_step = 0
 
         # Necessary for human mode
@@ -111,6 +81,31 @@ class CapEnv(gym.Env):
 
 
         return self.state
+
+    def _map_to_list(self, complete_map, static_map):
+        """
+        From given map, it generates objects of agents and push them to list
+
+        """
+        team_blue = []
+        team_red = []
+
+        for y in range(len(complete_map)):
+            for x in range(len(complete_map[0])):
+                if complete_map[x][y] == TEAM1_UGV:
+                    cur_ent = GroundVehicle([x, y], static_map, 1)
+                    team_blue.append(cur_ent)
+                elif complete_map[x][y] == TEAM1_UAV:
+                    cur_ent = AerialVehicle([x, y], static_map, 1)
+                    team_blue.insert(0, cur_ent)
+                elif complete_map[x][y] == TEAM2_UGV:
+                    cur_ent = GroundVehicle([x, y], static_map, 2)
+                    team_red.append(cur_ent)
+                elif complete_map[x][y] == TEAM2_UAV:
+                    cur_ent = AerialVehicle([x, y], static_map, 2)
+                    team_red.insert(0, cur_ent)
+
+        return team_blue, team_red
 
     def create_reward(self):
         """
@@ -122,11 +117,11 @@ class CapEnv(gym.Env):
             CapEnv object
         """
         reward = 0
-        # Win and loss return max rewards
-        # if self.game_lost:
-        # return -1
-        # if self.game_won:
-        # return 1
+
+        if self.blue_win:
+            return 100
+        if self.red_win:
+            return -100
 
         # Dead enemy team gives .5/total units for each dead unit
         for i in range(len(self.team_red)):
@@ -135,16 +130,6 @@ class CapEnv(gym.Env):
         for i in range(len(self.team_blue)):
             if not self.team_blue[i].isAlive:
                 reward -= (50.0 / len(self.team_blue))
-
-        # 10,000 steps returns -.5
-        # map_size_2 = map_size[0]*map_size[1]
-        # reward-=(.5/map_size_2)
-        #reward -= (50.0 / 1000) * self.cur_step
-        if self.game_won:
-            reward += 100
-        if reward <= -100:
-            reward = -100
-            self.game_lost = True
 
         return reward
 
@@ -344,34 +329,28 @@ class CapEnv(gym.Env):
                 has_alive_entity = True
                 locx, locy = i.get_loc()
                 if self.team_home[locx][locy] == TEAM1_FLAG:
-                    self.game_lost = True
-
+                    self.red_win = True
         # TODO Change last condition for multi agent model
         if not has_alive_entity and self.mode != "sandbox" and self.mode != "human_blue":
-            self.game_won = True
-            self.game_lost = False
+            self.blue_win = True
+
         has_alive_entity = False
         for i in self.team_blue:
             if i.isAlive and not i.air:
                 has_alive_entity = True
                 locx, locy = i.get_loc()
                 if self.team_home[locx][locy] == TEAM2_FLAG:
-                    self.game_lost = False
-                    self.game_won = True
+                    self.blue_win = True
         if not has_alive_entity:
-            self.game_lost = True
-            self.game_won = False
+            self.red_win = True
 
         reward = self.create_reward()
 
         self.create_observation_space()
-        # self.individual_reward()
-        # self.state = self.observation_space
-        self.state = self._env
 
-        isDone = False
-        if self.game_won or self.game_lost:
-            isDone = True
+        self.state = np.copy(self._env)
+
+        isDone = self.red_win or self.blue_win
         info = {}
 
         return self.state, reward, isDone, info
@@ -407,19 +386,28 @@ class CapEnv(gym.Env):
         map_h = len(env[0])
         map_w = len(env)
 
+        from gym.envs.classic_control import rendering
+
         self.viewer.draw_polygon([(0, 0), (SCREEN_W, 0), (SCREEN_W, SCREEN_H), (0, SCREEN_H)], color=(0, 0, 0))
 
         for row in range(map_h):
             for col in range(map_w):
-                cur_color = np.divide(COLOR_DICT[env[row][col]], 255)
-                if env[row][col] == TEAM1_UAV or env[row][col] == TEAM2_UAV:
-                    self.viewer.draw_circle(tile_w / 2, 20, color=cur_color).add_attr([col * tile_w, row * tile_h])
-                else:
-                    self.viewer.draw_polygon([
+                cur_color = np.divide(COLOR_DICT[env[col][row]], 255)
+                self.viewer.draw_polygon([
+                    (col * tile_w, row * tile_h),
+                    (col * tile_w + tile_w, row * tile_h),
+                    (col * tile_w + tile_w, row * tile_h + tile_h),
+                    (col * tile_w, row * tile_h + tile_h)], color=cur_color)
+
+                if env[col][row] == TEAM1_UAV or env[col][row] == TEAM2_UAV:
+                    self.viewer.draw_polyline([
                         (col * tile_w, row * tile_h),
-                        (col * tile_w + tile_w, row * tile_h),
-                        (col * tile_w + tile_w, row * tile_h + tile_h),
-                        (col * tile_w, row * tile_h + tile_h)], color=cur_color)
+                        ((col+1) * tile_w, (row+1) * tile_h)],
+                        color=(0,0,0), linewidth=2)
+                    self.viewer.draw_polyline([
+                        ((col+1) * tile_w, row * tile_h),
+                        (col * tile_w, (row+1) * tile_h)],
+                        color=(0,0,0), linewidth=2)#col * tile_w, row * tile_h
 
         return self.viewer.render(return_rgb_array = mode=='rgb_array')
 

gym_cap/gym_cap/envs/create_map.py
@@ -6,64 +6,93 @@ class CreateMap:
     given dimension size, number of obstacles,
     and number of agents for each team"""
 
-    def gen_map(name, dim=20, in_seed=None, map_obj=[4, NUM_BLUE, NUM_UAV, NUM_RED, NUM_UAV, 0]):
+    def gen_map(name, dim=20, in_seed=None, rand_zones=False,
+                map_obj=[NUM_BLUE, NUM_UAV, NUM_RED, NUM_UAV, NUM_GRAY]):
         """
-        0 <= in_seed <= 2**32
-
         Parameters
         ----------
-        name    : TODO
-            TODO
-        dim     : int
+        name        : TODO
+            Not used
+        dim         : int
             Size of the map
-        map_obj : list
+        in_seed     : int
+            Random seed between 0 and 2**32
+        rand_zones  : bool
+            True if zones are defined random
+        map_obj     : list
             The necessary elements to build the map
-            0   : obstacles
-            1   : player UGV
-            2   : player UAV
-            3   : enemy UGV
-            4   : enemy UAV
-            5   : gray units
+            0   : blue UGV
+            1   : blue UAV
+            2   : red UGV
+            3   : red UAV
+            4   : gray units
         """
+
+        # init the seed and set new_map to zeros
         if not in_seed == None:
             np.random.seed(in_seed)
         new_map = np.zeros([dim, dim], dtype=int)
 
-        # zones and obstacles init
-        new_map[:, 0:dim//2] = TEAM1_BACKGROUND
-        new_map[:, dim//2:dim] = TEAM2_BACKGROUND
+        # zones init
+        new_map[:,:] = TEAM2_BACKGROUND
+        if rand_zones:
+            sx, sy = np.random.randint(dim//2, 4*dim//5, [2])
+            lx, ly = np.random.randint(0, dim - max(sx,sy)-1, [2])
+            new_map[lx:lx+sx, ly:ly+sy] = TEAM1_BACKGROUND
+        else:
+            new_map[:,0:dim//2] = TEAM1_BACKGROUND
 
-        for i in range(map_obj[0]):
+        # obstacles init
+        num_obst = int(np.sqrt(dim))
+        for i in range(num_obst):
             lx, ly = np.random.randint(0, dim, [2])
             sx, sy = np.random.randint(0, dim//5, [2]) + 1
             new_map[lx-sx:lx+sx, ly-sy:ly+sy] = OBSTACLE
 
         # define location of flags
-        new_map = CreateMap.gen_random(new_map,
+        new_map = CreateMap.populate_map(new_map,
                              TEAM1_BACKGROUND, TEAM1_FLAG)
-        new_map = CreateMap.gen_random(new_map,
+        new_map = CreateMap.populate_map(new_map,
                              TEAM2_BACKGROUND, TEAM2_FLAG)
 
-        for i in range(map_obj[1]):
-            new_map = CreateMap.gen_random(new_map,
+        # the static map is ready
+        static_map = np.copy(new_map)
+
+        for i in range(map_obj[0]):
+            new_map = CreateMap.populate_map(new_map,
                                  TEAM1_BACKGROUND, TEAM1_UGV)
-        for i in range(map_obj[3]):
-            new_map = CreateMap.gen_random(new_map,
-                                 TEAM2_BACKGROUND, TEAM2_UGV)
-        for i in range(map_obj[2]):
-            new_map = CreateMap.gen_random(new_map,
+        for i in range(map_obj[1]):
+            new_map = CreateMap.populate_map(new_map,
                                  TEAM1_BACKGROUND, TEAM1_UAV)
-        for i in range(map_obj[4]):
-            new_map = CreateMap.gen_random(new_map,
+        for i in range(map_obj[2]):
+            new_map = CreateMap.populate_map(new_map,
+                                 TEAM2_BACKGROUND, TEAM2_UGV)
+        for i in range(map_obj[3]):
+            new_map = CreateMap.populate_map(new_map,
                                  TEAM2_BACKGROUND, TEAM2_UAV)
 
-        #np.save('map.npy', new_map)
-        return new_map
+        # TODO: change zone for grey team to complete map
+        for i in range(map_obj[4]):
+            new_map = CreateMap.populate_map(new_map,
+                                 TEAM2_BACKGROUND, TEAM3_UGV)
 
-    def gen_random(new_map, code_where, code_what):
-        dim = new_map.shape[0]
+        #np.save('map.npy', new_map)
+        return new_map, static_map
+
+    def populate_map(new_map, code_where, code_what):
+        """
+        Adds "code_what" to a random location of "code_where" at "new_map"
+        new_map     : 2d numpy array
+            Map of the environment
+        code_where  : int
+            Code of the territory that is being populated
+        code_what   : int
+            Value assigned to the random location of the map
+        """
+        dimx, dimy = new_map.shape
         while True:
-            lx, ly = np.random.randint(0, dim, [2])
+            lx = np.random.randint(0, dimx)
+            ly = np.random.randint(0, dimy)
             if new_map[lx,ly] == code_where:
                 break
         new_map[lx,ly] = code_what