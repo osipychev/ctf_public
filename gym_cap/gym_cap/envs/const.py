c03d029038c98c20e8d7ec386b8d93a414da8ff5
Denis Osipychev <denisosipychev@eduroamprvnat-172-16-22-207.near.illinois.edu>
2018-10-17 20:14:22 -0500

    added random territory generator. functions moved. added grey team generator. fixed reward system. fixed bug with rendering - uavs and rotation

--- gym_cap/gym_cap/envs/const.py
+++ gym_cap/gym_cap/envs/const.py
@@ -8,7 +8,7 @@ GRAY = 90
 NUM_BLUE = 4
 NUM_RED = 4
 NUM_UAV = 0
-NUM_GRAY = 4
+NUM_GRAY = 0
 UAV_STEP = 3
 UGV_STEP = 1
 UAV_RANGE = 4
@@ -49,6 +49,7 @@ OBSTACLE = 8
 DEAD = 9
 SELECTED = 10
 COMPLETED = 11
+TEAM3_UGV = 15
 
 COLOR_DICT = {UNKNOWN : (200, 200, 200),
               TEAM1_BACKGROUND : (0, 0, 120),
@@ -60,6 +61,7 @@ COLOR_DICT = {UNKNOWN : (200, 200, 200),
               TEAM1_FLAG : (0, 255, 255),
               TEAM2_FLAG : (255, 255, 0),
               OBSTACLE : (120, 120, 120),
+              TEAM3_UGV : (180, 180, 180),
               DEAD : (0, 0, 0),
               SELECTED : (122, 77, 25),
               BLACK : (0, 0, 0),
