cdbb32195e8f8ac496e08ae1fb0bc3b548f3b241
Shamith Achanta <43729418+shamith2@users.noreply.github.com>
2019-05-20 11:42:57 -0500

    added feature to close gym and restart environment (#28)
    * added feature to close gym and restart environment
    
    added feature to close gym and restart environment by using 'keyboard interrupt'
    
    * add close message

--- cap_test.py
+++ cap_test.py
@@ -22,31 +22,38 @@ observation = env.reset(map_size=20,
                         policy_blue=policy.roomba.PolicyGen(env.get_map, env.get_team_blue),
                         policy_red=policy.random.PolicyGen(env.get_map, env.get_team_red))
 
-while True:
-    while not done:
+try:
+    while True:
+        while not done:
+    
+            #you are free to select a random action
+            # or generate an action using the policy
+            # or select an action manually
+            # and the apply the selected action to blue team
+            # or use the policy selected and provided in env.reset
+            #action = env.action_space.sample()  # choose random action
+            #action = policy_blue.gen_action(env.team1,observation,map_only=env.team_home)
+            #action = [0, 0, 0, 0]
+            #observation, reward, done, info = env.step(action)
+    
+            observation, reward, done, info = env.step()  # feedback from environment
+    
+            # render and sleep are not needed for score analysis
+            env.render()
+            time.sleep(.05)
+    
+            t += 1
+            if t == 100:
+                break
+    
+        env.reset()
+        done = False
+        rscore.append(reward)
+        print("Time: %.2f s, score: %.2f" %
+            ((time.time() - start_time),np.asarray(rscore).mean()))
+                
+except KeyboardInterrupt:
+    env.close()
+    del gym.envs.registry.env_specs['cap-v0']
 
-        #you are free to select a random action
-        # or generate an action using the policy
-        # or select an action manually
-        # and the apply the selected action to blue team
-        # or use the policy selected and provided in env.reset
-        #action = env.action_space.sample()  # choose random action
-        #action = policy_blue.gen_action(env.team1,observation,map_only=env.team_home)
-        #action = [0, 0, 0, 0]
-        #observation, reward, done, info = env.step(action)
-
-        observation, reward, done, info = env.step()  # feedback from environment
-
-        # render and sleep are not needed for score analysis
-        env.render()
-        time.sleep(.05)
-
-        t += 1
-        if t == 100:
-            break
-
-    env.reset()
-    done = False
-    rscore.append(reward)
-    print("Time: %.2f s, score: %.2f" %
-        ((time.time() - start_time),np.asarray(rscore).mean()))
+    print("CtF environment Close")
